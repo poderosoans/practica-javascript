859cce01d884dffaac8f28e98e4ba2c21e42754a
Roger Pomacarhua <poderosoans@hotmail.com>
2017-08-27 20:17:58 -0500

    Ambiente configurado para trabajar con Date (fechas)

---
+++ fechas/date.js
@@ -0,0 +1,27 @@
+// Date => Es un objeto que requiere ser instancia para usarse
+
+	//let date = new Date(),
+	//date2 = new Date();
+	//date.setFullYear(1980);
+	//date.setMinutes(58);
+	//date.setHours(20);
+	//date.setDate(21);
+	//date.setMonth(11); // De 0 a 11 porque es un Array
+	//console.log(date);
+	//console.log(date2);
+
+// Métodos get en el objeto Date
+// .getFullYear()
+// .getMinutes()
+// .getMonth() => 0 es enero
+// .getHours() => 0 - 24
+// .getSeconds()
+// .getMilliseconds()
+// .getDate() => Día del mes
+// .getDay() => Día de la semana (0 es domingo)
+// .getTime() => Milisegundos desde el 1 de enero de 1970
+
+ 	//let date = new Date();
+	//console.log(date.getTime());
+
+// Ejercicio 1: Imprimir la fecha y hora en formato personalizado
