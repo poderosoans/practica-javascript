a7c754cdd1a3828649f1fdf5a0fdd5e44f7c4728
Roger Pomacarhua <poderosoans@hotmail.com>
2017-09-19 12:48:14 -0500

    Primeros pasos para el desarrollo de la galería e inserción de imágenes

---
+++ js-pyfinal/dev/js/lightbox.js
@@ -0,0 +1,7 @@
+// Al hacer clic en una imagen se debe abrir su versión grande
+	// se abre en un overlay
+	// Cerrar el overlay
+
+// Al abrirse la versión grande debe tener
+	// Descripción de la imagen (que se tomará del atributo alt)
+	// Navegación entre la imagen (siguiente y anterior)
